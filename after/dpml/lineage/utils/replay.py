0ef8b36e59f83bc9c0c0e55acb58dc32e975b86e
fabriceyhc <fabriceyhc@gmail.com>
2022-07-19 22:08:42 -0700

    Update replay.py

--- after/dpml/lineage/utils/replay.py
+++ after/dpml/lineage/utils/replay.py
@@ -1,8 +1,19 @@
+import inspect
+
 def full_module_name(o):
-    module = o.__class__.__module__
-    if module is None or module == str.__class__.__module__:
-        return o.__class__.__name__
-    return module, o.__class__.__name__
+    """
+    returns module_name (str), class_name (str)
+    """
+    if inspect.ismethod(o):
+        return o.__self__.__module__, o.__self__.__class__.__name__
+    elif inspect.isfunction(o):
+        return o.__module__, None
+    elif inspect.isclass(o):
+        return o.__module__, o.__name__
+    elif inspect.isclass(type(o)):
+        return type(o).__module__, type(o).__name__    
+    else:
+        raise ValueError(f"Unable to parse {o} ({type(o)})")
 
 def dynamic_import(module_name, class_name):
     

f22c4a55833dd9bf4664995436e742a509f713da
Fabrice Harel-Canada <fabriceyhc@gmail.com>
2022-09-19 21:11:25 -0700

    Update textfetch.py

--- after/dpml/lineage/search/textfetch.py
+++ after/dpml/lineage/search/textfetch.py
@@ -1,4 +1,5 @@
 import numpy as np
+from collections import defaultdict 
 from textdiversity import (
     DocumentSemanticDiversity,
     POSSequenceDiversity,
@@ -9,6 +10,7 @@ from textdiversity import (
 class TextFetch:
     def __init__(self, texts: list) -> None:
         self.texts = texts
+        self.text_ids = []
 
         # featurizers
         self.semantic_featurizer = DocumentSemanticDiversity()
@@ -26,27 +28,31 @@ class TextFetch:
         self.semantic_text = None
         self.syntactic_text = None
         self.morphological_text = None
-        self.phonological_features = None
+        self.phonological_text = None
 
     def compute_semantic_features(self) -> None:
-        features, texts = self.semantic_featurizer.extract_features(self.texts)
+        features, texts, text_ids = self.semantic_featurizer.extract_features(self.texts, return_ids=True)
         self.semantic_features = features
         self.semantic_text = texts
+        self.semantic_text_ids = text_ids
 
     def compute_syntactic_features(self) -> None:
-        features, texts = self.syntactic_featurizer.extract_features(self.texts)
+        features, texts, text_ids = self.syntactic_featurizer.extract_features(self.texts, return_ids=True)
         self.syntactic_features = features
         self.syntactic_text = texts
+        self.syntactic_text_ids = text_ids
 
     def compute_morphological_features(self) -> None:
-        features, texts = self.morphological_featurizer.extract_features(self.texts)
+        features, texts, text_ids = self.morphological_featurizer.extract_features(self.texts, return_ids=True)
         self.morphological_features = features
         self.morphological_text = texts
+        self.morphological_text_ids = text_ids
 
     def compute_phonological_features(self) -> None:
-        features, texts = self.phonological_featurizer.extract_features(self.texts)
+        features, texts, text_ids = self.phonological_featurizer.extract_features(self.texts, return_ids=True)
         self.phonological_features = features
         self.phonological_features = texts
+        self.phonological_text_ids = text_ids
 
     def compute_features(self) -> None:
         self.compute_semantic_features()
@@ -54,26 +60,35 @@ class TextFetch:
         self.compute_morphological_features()
         # self.compute_phonological_features()
 
-
-    def search(self, query: str, linguistic_type: str = "semantic", top_n: int = 1):
+    def get_linguistic_data(self, linguistic_type: str):
         if "semantic" in linguistic_type:
-            ranker = self.semantic_featurizer
+            ranker  = self.semantic_featurizer
             t_feats = self.semantic_features
             t_texts = self.semantic_text
+            t_ids   = self.semantic_text_ids
         elif "syntactic" in linguistic_type:
-            ranker = self.syntactic_featurizer
+            ranker  = self.syntactic_featurizer
             t_feats = self.syntactic_features
             t_texts = self.syntactic_text
+            t_ids   = self.syntactic_text_ids
         elif "morphological" in linguistic_type:
-            ranker = self.morphological_featurizer
+            ranker  = self.morphological_featurizer
             t_feats = self.morphological_features
             t_texts = self.morphological_text
+            t_ids   = self.morphological_text_ids
         elif "phonological" in linguistic_type:
-            ranker = self.phonological_featurizer
+            ranker  = self.phonological_featurizer
             t_feats = self.phonological_features
             t_texts = self.phonological_text
+            t_ids   = self.phonological_text_ids
         else:
-            raise ValueError("Invalid linguistic_type provided.")
+            raise ValueError(f"Invalid linguistic_type: {linguistic_type}")
+
+        return ranker, t_feats, t_texts, np.array(t_ids, dtype=np.int32)
+
+    def search_for_text(self, query: str, linguistic_type: str = "semantic", top_n: int = 1):
+
+        ranker, t_feats, t_texts, t_ids = self.get_linguistic_data(linguistic_type)
 
         if top_n == -1:
             top_n = len(t_texts)
@@ -84,11 +99,36 @@ class TextFetch:
 
         # rank based on similarity
         rank_idx = np.argsort(z)[::-1]
+        t_ids = t_ids[rank_idx]
 
         ranking = np.array(t_texts)[rank_idx].tolist()
         scores = z[rank_idx]
 
         return ranking[:top_n], scores[:top_n]
+
+    def search_for_ids(self, query: str, linguistic_type: str = "semantic", top_n: int = 1):
+
+        ranker, t_feats, t_texts, corpus_ids = self.get_linguistic_data(linguistic_type)
+        
+        corpus_tuples = [(corpus_id, sentence_id) for sentence_id, corpus_id in enumerate(corpus_ids)]
+        corpus_map = defaultdict(list)
+        for (corpus_id, sentence_id) in corpus_tuples:
+            corpus_map[corpus_id].append(sentence_id)
+
+        if top_n == -1:
+            top_n = len(t_texts)
+
+        q_feats, q_texts = ranker.extract_features([query])
+
+        z = ranker.calculate_similarity_vector(q_feats[0], t_feats)
+
+        # rank based on similarity
+        rank_idx = np.argsort(z)[::-1]
+        t_ids = np.array(corpus_ids)[rank_idx]
+        corpus_ids = list(dict.fromkeys(t_ids))
+        corpus_scores = [z[corpus_map[id]].mean() for id in corpus_ids]
+
+        return corpus_ids[:top_n], corpus_scores[:top_n]
 
 if __name__ == "__main__":
 
@@ -96,7 +136,7 @@ if __name__ == "__main__":
     from time import perf_counter
     from datasets import load_dataset
 
-    dataset = load_dataset("glue", "sst2", split="train[:1000]")
+    dataset = load_dataset("glue", "sst2", split="train[:100]")
     dataset = dataset.rename_column("sentence", "text")
 
     text_fetcher = TextFetch(dataset['text'])
@@ -111,13 +151,23 @@ if __name__ == "__main__":
 
     linguistic_features = ["semantic", "syntactic", "morphological"] #, "phonological"]
 
+    print("search_for_text")
     for lf in linguistic_features:
         print()
         start_time = perf_counter()
-        ranking, scores = text_fetcher.search(query, lf, top_n=3)
+        ranking, scores = text_fetcher.search_for_text(query, lf, top_n=3)
         print(f"{lf} search ({round(perf_counter() - start_time, 2)}s)")
         for text, score in zip(ranking, scores):
             print(f"score: {round(score, 2)} | text: {text}")
+
+    print("search_for_ids")
+    for lf in linguistic_features:
+        print()
+        start_time = perf_counter()
+        corpus_ids, scores = text_fetcher.search_for_ids(query, lf, top_n=3)
+        print(f"{lf} search ({round(perf_counter() - start_time, 2)}s)")
+        for id, score in zip(corpus_ids, scores):
+            print(f"score: {round(score, 2)} | id: {id} | text: {dataset['text'][id]}")  
 
     # (dpml) ~\dpml\lineage\search>python textfetch.py
     # precomputation took 16.57 seconds
